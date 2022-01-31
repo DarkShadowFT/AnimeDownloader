a64ec6b2d4676d4ad44f1afb8a57f371d4d4c910
DarkShadowFT <41124884+DarkShadowFT@users.noreply.github.com>
2022-02-01 00:06:56 +0500

    Added exception handling

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from importlib.metadata import files
+import atexit
 import os
 import time
 from datetime import datetime
@@ -15,10 +16,16 @@ def new_ep(f, ep_num):
 		for i in range(0, len(file_nums)):
 			if file_nums[i] == str(ep_num):
 				return False
-	return True
+	return True	
+
+
+def cleanup():
+	driver.quit()
+	print("Exiting")
 
 
 def download_anime():
+	ep_downloaded = False
 	path = "C:\\Users\\r2fpdvzocm5ydw\\PycharmProjects\\AnimeDownloader\\Anime_Files"
 	files, flags = [], []
 	anime_names = open(os.path.join(path, "anime_names.txt"), "r")
@@ -38,25 +45,28 @@ def download_anime():
 		search.send_keys(Keys.RETURN)
 		names = driver.find_element(By.TAG_NAME, 'tbody').find_elements(By.TAG_NAME, 'tr')
 		for i in range(0, len(names)):
-			name = names[i].text[names[i].text.find(anime_names[j]):]
+			animes = names[i].find_elements(By.TAG_NAME, 'td')
+			name = animes[1].text[animes[1].text.find(anime_names[j]):]
 			nums = [int(s) for s in name.split() if s.isdigit()]
-			ep_num = int(nums[0])
+			if len(nums):
+				ep_num = int(nums[0])
 
-			if name.lower().find(anime_names[j].lower()) != -1:
-				flags[j] = True
-				new_ep_found = new_ep(files[j], ep_num)
+				if name.lower().find(anime_names[j].lower()) != -1:
+					flags[j] = True
+					new_ep_found = new_ep(files[j], ep_num)
 
-			if new_ep_found:
-				anime = names[i].find_elements(By.TAG_NAME, 'td')
-				links = anime[2].find_elements(By.TAG_NAME, 'a')
-				links[1].click()
-				print("Clicked on the magnet link of " + name)
-				files[j].write(str(ep_num) + "\n")
+				if new_ep_found:
+					anime = names[i].find_elements(By.TAG_NAME, 'td')
+					links = anime[2].find_elements(By.TAG_NAME, 'a')
+					links[1].click()
+					print("Clicked on the magnet link of " + name)
+					ep_downloaded = True
+					files[j].write(str(ep_num) + "\n")
 		files[j].close()
 	
-	if not new_ep_found:
-		current_time = datetime.now().strftime("%H:%M:%S")
-		print("No new ep released - " + current_time)
+	if not ep_downloaded:
+		current_time = datetime.now().strftime("%d %b, %Y, %H:%M:%S")
+		print("\nNo new ep released - " + current_time)
 
 
 os.environ['WDM_LOG_LEVEL'] = '0'
@@ -69,10 +79,15 @@ options.add_argument('--profile-directory=Profile 1') #e.g. Profile 3
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 driver.maximize_window()
 url = "https://nyaa.si/"
+atexit.register(cleanup)
 
 while not False:
-	driver.get(url)
-	download_anime()
-	time.sleep(1200)
-
-driver.close()
+	try:
+		driver.get(url)
+		download_anime()
+	except Exception as e:
+		print(e)
+		print("Restarting")
+		continue
+	else:
+		time.sleep(1200)
